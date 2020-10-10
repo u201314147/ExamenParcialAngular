20108def77231a11f2d08afac903f4ea95494a9a
u201314147 <u201314147@upc.edu.pe>
2020-10-10 16:25:00 -0500

    segunda version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'pm-root',
+  template: `
+    <nav class="navbar navbar-expand navbar-light bg-light">
+      <a class="navbar-brand">{{ pageTitle }}</a>
+      <ul class="nav nav-pills">
+        <li>
+          <a
+            class="nav-link"
+            routerLinkActive="active"
+            [routerLink]="['/welcome']"
+          >
+            Home
+          </a>
+        </li>
+        <li>
+          <a
+            class="nav-link"
+            routerLinkActive="active"
+            [routerLink]="['/products']"
+          >
+            Product List
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <div class="container">
+      <router-outlet></router-outlet>
+    </div>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'products-angular';
+  pageTitle = 'Product Management'
 }
